ef1d3f129e777fe59f12995832c1c3017a66951a
mkaltenbrunnerFH <56868226+mkaltenbrunnerFH@users.noreply.github.com>
2019-11-25 10:31:21 +0100

    Add files via upload
    Upload testfile.py mit Wigle Vergleich und Darstellung auf Karte

---
+++ testfile.py
@@ -0,0 +1,69 @@
+from scapy.all import *
+from json2table import convert
+ 
+import requests
+import json
+import sys
+
+# IMPORTING REQUIRED LIBRARIES:
+import requests
+from requests.auth import HTTPBasicAuth
+import pandas as pd
+from pandas.io.json import json_normalize
+import matplotlib.pyplot as plt
+import geoplotlib as gp
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# using API to checkout vendors by MAC
+url = 'http://macvendors.co/api/%s'
+
+# using API from wigle
+url2 = 'https://api.wigle.net/api/v2/network/search'
+
+probe_data = set()
+beacon_data = set()
+
+def search_prob(p):
+    if (p.type == 0) and\
+    (p.subtype == 4) and\
+    (p.info != b''):
+        mac_and_ssid = (p.addr2,p.info)
+        if (mac_and_ssid not in probe_data):
+          r = requests.get(url % p.addr2)
+          req = json.loads(r.content)
+          print("PrbReq, SSID = %s, Src = %s, Vendor = %s" %(p.info, p.addr2, req['result']['company']))
+          probe_data.add(mac_and_ssid)
+	  
+	  r2 = requests.get(url2, {'ssid':p.info}, auth=('AID0c03e6a1fdff1332213ca04110bf6cb8', '6f7ae03341051392258941eda733365c'))
+          #print('test')
+	  json_object = {"key" : "value"}
+	  build_direction = "LEFT_TO_RIGHT"
+          table_attributes = {"style" : "width:100%"}
+          html = convert (r2.json(), build_direction=build_direction, table_attributes=table_attributes)
+	  #print(html)
+          #'<table style="width:100%"><tr><th>key</th><td>value</td></tr></table>'
+		
+          #print(r2.json())
+
+          details = r2.json()
+
+          # EXTRACTING 'RESULTS' AS A PANDAS DATAFRAME TO WORK WITH:
+          df = json_normalize(details['results'])
+
+          # RENAMING COLUMNS FOR GEOPLOTLIB:
+          df = df.rename(columns={'trilat': 'lat', 'trilong': 'lon'})
+          cols = list(df.columns) 
+       
+          # PREVIEWING AVAILABLE INFORMATION:
+          print("Result obtained has {df.shape[0]} rows and {df.shape[1]} columns in it. \n\nThe list of columns include {cols}")
+
+          print(df)
+
+          # GENERATING WIFI SPOTS MAP OF NETWORKS:
+          gp.dot(df)
+          gp.show()
+
+
+scapy.all.sniff(iface=sys.argv[1],prn=search_prob)
